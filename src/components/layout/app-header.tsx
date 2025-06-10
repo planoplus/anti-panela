6e22937ac4fdee4ccece449f433af154b7ed37e1
Dellanio Alencar <dellanio@gmail.com>
2025-06-10 04:09:33 +0000

    reduza o tamanho do Texto BraLiga - Edition: Anti-Panela em 20% de forma

--- src/components/layout/app-header.tsx
+++ src/components/layout/app-header.tsx
@@ -6,15 +6,15 @@ export function AppHeader() {
     <header className="py-6 text-center text-white shadow-md bg-black/20">
       <div className="container mx-auto">
         <div className="flex flex-col sm:flex-row items-center justify-center mb-1">
-          <Image 
-            src="https://placehold.co/212x80.png" 
-            alt="BraLiga Logo" 
-            width={212} 
-            height={80} 
+          <Image
+            src="https://placehold.co/212x80.png"
+            alt="BraLiga Logo"
+            width={212}
+            height={80}
             className="mr-0 sm:mr-3 mb-2 sm:mb-0"
             data-ai-hint="soccer logo"
           />
-          <h1 className="text-4xl md:text-5xl font-headline">
+          <h1 className="text-3xl md:text-4xl font-headline">
             BraLiga - <span className="text-white">Edition: Anti-Panela</span>
           </h1>
         </div>
